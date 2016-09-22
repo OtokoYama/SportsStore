7c88b27ac3dc9ff17465e12bd437d9b413db7e14
Rick Hysell <rhysell@fema-corp.com>
2016-09-22 15:08:09 -0400

    initial project commit

---
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+
+// This is the repository class. It implements the IProductRepository interface and uses an instance of
+// EFDbContext to retrieve data from the database using the Entity Framework
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
